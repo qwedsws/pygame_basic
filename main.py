03da6389b6e93514cc753bd7b738667a0486d4d1
qwedsws <67382967+qwedsws@users.noreply.github.com>
2022-11-01 23:46:02 -0400

    neccesary file

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400)) #width,height
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('Red')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Black')
 
 while True:
     for event in pygame.event.get():
@@ -15,7 +17,9 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface,(0,0))
+    screen.blit(sky_surface,(0,0)) # 0,0 is top left of the surface. its like x,y axis
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface,(5,5))
 
     pygame.display.update()
     clock.tick(60) # 60FPS limits While loop
